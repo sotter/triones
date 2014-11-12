052b4a9e813f3fdd9be8d13321838e8c546b4b63
BotterLiu <liubo060807@163.com>
2014-11-12 15:20:53 +0800

    test

--- src/net/transport.cpp
+++ src/net/transport.cpp
@@ -1,16 +1,4 @@
-/*
- * (C) 2007-2010 Taobao Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * Version: $Id$
- *
- * Authors:
- *   duolong <duolong@taobao.com>
- *
- */
+
 #include "cnet.h"
 #include "../comm/comlog.h"
 #include "tbtimeutil.h"
